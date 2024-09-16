f200a91677ddca674283266f341803702f2830c5
aguiarpedro12 <pedraum35aguiar@gmail.com>
2024-09-16 15:16:20 -0300

    first commit

---
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -0,0 +1,90 @@
+import { useContext, useState, useEffect } from 'react'
+import './ProjectsList.css'
+
+//ASSETS
+import LikeFilled from '../../assets/like-filled.svg'
+import LikeOutline from '../../assets/like.svg'
+
+// COMPONENTS
+import Button from '../Button/Button'
+
+//CONTEXT
+import { AppContext } from '../../contexts/AppContext'
+
+//UTILS
+import { getApiData } from '../../services/apiServices'
+
+
+function ProjectsList() {
+    const [projects, setProjects] = useState()
+    const [favProjects, setFavProject] = useState([])
+    const appContext = useContext(AppContext)
+
+    const handleSavedProjects = (id) => {
+        setFavProject((prevFavProjects) => {
+            if(prevFavProjects.includes(id)) {
+                const filterArray = prevFavProjects.filter((projectId) => projectId !== id)
+                sessionStorage.setItem('favProjects', JSON.stringify(filterArray))
+                return prevFavProjects.filter((projectId) => projectId !== id)
+            } else {
+                sessionStorage.setItem('favProjects', JSON.stringify([...prevFavProjects, id]))
+                return [...prevFavProjects, id]
+            }
+        })
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log('Fetching data...');
+            try {
+                const projectsResponse = await getApiData('projects')
+                console.log('Response:', projectsResponse);
+                setProjects(projectsResponse)
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setProjects([])
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    useEffect(() => {
+        const savedFavProjects = JSON.parse(sessionStorage.getItem('favProjects'))
+        if (savedFavProjects) {
+            setFavProject(savedFavProjects)
+        }
+    }, [])
+
+    return (
+        <div className="projects-section">
+            <div className="projects-hero">
+                <h2>{appContext.languages[appContext.language].projects.title}</h2>
+                <p>{appContext.languages[appContext.language].projects.subtitle}</p>
+            </div>
+            <div className="projects-grid">
+                {
+                    projects ?
+                        projects.map((project) =>(
+                            <div 
+                            className="project-card d-flex jc-center al-center fd-column" key={project.id}>
+                                    <div 
+                                        className="thumb tertiary-background"
+                                        style={{backgroundImage: `url(${project.thumb})`}}    
+                                    ></div>
+                                            <h3>{project.title}</h3>
+                                            <p>{project.subtitle}</p>
+                                            <Button buttonStyle="unstyled" onClick={() => handleSavedProjects(project.id)}>
+                                            <img src={favProjects.includes(project.id) ? LikeFilled : LikeOutline} height="20px" />
+                                            </Button>
+                            </div>
+                        )) 
+                    : 
+                    null
+                }
+            </div>
+        </div>
+    )
+}
+
+export default ProjectsList
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react'
+import Banner from '../components/Banner/Banner'
+import Footer from "../components/Footer/Footer"
+import ProjectsList from "../components/ProjectsList/ProjectsList"
+import Header from "../components/Header/Header"
+
+//CONTEXT
+import { AppContext } from '../contexts/AppContext'
+
+
+function Projects() {
+    const appContext = useContext(AppContext)
+    return(
+        <>
+        <Header />
+        <Banner title={appContext.languages[appContext.language].menu.projects} image="projects.jpg"/>
+        <div className="container">
+            <ProjectsList />
+        </div>
+        <Footer />
+        </>
+    )
+}
+
+export default Projects
